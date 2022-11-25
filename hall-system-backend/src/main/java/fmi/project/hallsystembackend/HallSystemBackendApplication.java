cd7d404669f6df6bbed8cb8ac3471984713de0e5
Vesela Petrova <petrovavesela00@gmail.com>
2022-11-25 15:04:53 +0200

    initial backend commit

---
+++ hall-system-backend/src/main/java/fmi/project/hallsystembackend/HallSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package fmi.project.hallsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HallSystemBackendApplication.class, args);
+    }
+
+}
---
+++ hall-system-backend/src/test/java/fmi/project/hallsystembackend/HallSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.project.hallsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
