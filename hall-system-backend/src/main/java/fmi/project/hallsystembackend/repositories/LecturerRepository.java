b260a5aae7a334b929da5d5bcdb1b0fd4a3d3262
Vesela Petrova <veselapetrova8900@gmail.com>
2023-01-14 01:13:07 +0200

    created delete endpoint

--- hall-system-backend/src/main/java/fmi/project/hallsystembackend/repositories/LecturerRepository.java
+++ hall-system-backend/src/main/java/fmi/project/hallsystembackend/repositories/LecturerRepository.java
@@ -1,13 +1,23 @@
 package fmi.project.hallsystembackend.repositories;
 
 import fmi.project.hallsystembackend.models.Lecturer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
+import java.time.LocalDate;
 import java.util.Set;
 
 @Repository
 public interface LecturerRepository extends CrudRepository<Lecturer, Long> {
     Lecturer findLecturerByName(String name);
     Set<Lecturer> findAll();
+
+    @Query(
+            value = "SELECT DISTINCT reservation_data.id FROM reservation_data " +
+                    "WHERE reservation_data.lecturer_id = :lecturerId AND reservation_data.hall_id = :hallId AND reservation_data.reserved_hour = :hour AND reservation_data.reserved_date = :date",
+            nativeQuery=true
+    )
+    Long getReservationId(@Param("lecturerId") Long lecturerId, @Param("hallId") Long hallId, @Param("hour") Integer reservedHour, @Param("date") LocalDate reservedDate);
 }
